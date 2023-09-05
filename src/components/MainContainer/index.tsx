f86e20b77b588b916bb57318d88e23777fe37801
Bruno de Sá <brunoviniciussouza.sa@gmail.com>
2023-09-05 17:06:36 -0300

    FIX: Started css corrections.

--- src/components/MainContainer/index.tsx
+++ src/components/MainContainer/index.tsx
@@ -8,16 +8,19 @@ interface MainContainerProps {
 }
 
 export function MainContainer(props: MainContainerProps) {
+  console.log(props.desktopImageUrl);
   return (
     <>
       <style>
         {`
           .main-container {
             background-image: url('${props.mobileImageUrl}');                  
+            background-size: cover;
           }
           @media (min-width: 1100px) { 
             .main-container {   
               background-image: url('${props.desktopImageUrl}');
+              background-size: cover;
             }
           }
         `}
